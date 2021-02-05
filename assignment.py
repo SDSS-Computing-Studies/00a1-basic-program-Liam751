b6a13cdeae50dcc44463abc57286c6593a091df0
Liam751 <55156691+Liam751@users.noreply.github.com>
2021-02-05 09:56:20 -0800

    assignment

--- assignment.py
+++ assignment.py
@@ -1,3 +1,8 @@
 #! python3
 
 # SD Computing Studies Assignment
+
+print("Hello World!")
+print("This is my second program.")
+print("It uses \"two commands\" to display the output.")
+print("Hello world! This is how multiple print statements are joined. Use end="end=")
